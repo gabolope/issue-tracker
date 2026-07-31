933cc48c6c7c2f29ab332c321a514af9c1b2aa63
Gabriel Alejandro López <78502447+gabolope@users.noreply.github.com>
2026-07-31 13:55:11 -0300

    Connected to MySQL database in Aiven

--- prisma/client.ts
+++ prisma/client.ts
@@ -1,21 +1,11 @@
 import "dotenv/config";
 import { PrismaClient } from "@prisma/client";
-import { PrismaMariaDb } from "@prisma/adapter-mariadb";
-
-const adapter = new PrismaMariaDb({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "1234",
-  database: "issue-tracker",
-  connectionLimit: 5,
-});
 
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;
 };
 
-const prisma = globalForPrisma.prisma ?? new PrismaClient({ adapter });
+const prisma = globalForPrisma.prisma ?? new PrismaClient();
 
 if (process.env.NODE_ENV !== "production") {
   globalForPrisma.prisma = prisma;
